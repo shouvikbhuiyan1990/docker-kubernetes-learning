39ea004e11256bc4641123ef4fd4e2d943c441f5
Shouvik Bhuiyan <shouvikbhuiyan1990@gmail.com>
2021-07-03 08:21:07 +0530

    init server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const redis = require('redis');
+const { Pool } = require('pg');
+
+const client = redis.createClient();
+const redisPublisher = client.duplicate();
+
+const pgClient = new Pool({
+    user: 'shouvikbhuiyan',
+    host: 'localhost',
+    database: 'testdb',
+    password: 'password',
+    port: 5432,
+});
+  
+pgClient.on("connect", (client) => {
+client
+    .query("CREATE TABLE IF NOT EXISTS colors (color VARCHAR, id VARCHAR)")
+    .catch((err) => console.error(err));
+});
 
 const app = express();
 
@@ -8,7 +27,47 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send('Hi, Express server is running');
+    // client.hmset('frameworks', 'javascript', 'AngularJS', 'css', 'Bootstrap', 'node', 'Express');
+
+    // client.hgetall('frameworks', function(err, object) {
+    //     console.log(object);
+    //     res.send(object);
+    // });
+
+    res.send('Hi! these are very simple apis to create multi container docker');
+});
+
+app.post('/api/closest/colors', async (req, res) => {
+    const color = req.body.color;
+    await pgClient.query("INSERT INTO colors(color, id) VALUES($1, $2)", [color, new Date().toString()]);
+    client.hmget('colors', color, function(err, result) {
+        console.log('Results are ',result);
+        if (result && result[0]) {
+            res.send({
+                result
+            });
+        }
+        else {
+            redisPublisher.publish('insert', color);
+            res.send({
+                result: []
+            });
+        }
+    });
+});
+
+app.get('/api/get/allcolors', (req, res) => {
+    client.hgetall('colors', function(err, object) {
+        res.send(object);
+    });
+});
+
+app.get('/api/recentSearches', async (req, res) => {
+    const values = await pgClient.query("SELECT * FROM colors ORDER BY id DESC LIMIT 2");
+
+    res.send({
+        result: values.rows
+    });
 });
 
 
---
+++ worker/index.js
@@ -0,0 +1,36 @@
+const redis =  require('redis');
+
+const client = redis.createClient();
+const sub = client.duplicate();
+
+function findClosestColors(current) {
+    const validColorRegex = /^[0-9A-F]{6}$/i;
+    let validColors = [],
+        limit = 200,
+        dec = parseInt(current, 16),
+        generated = 0;
+    
+    while (validColors.length < 10 && limit >= 0) {
+        let randomModifier = Math.floor(Math.random() * (100 - 2 + 1) + 2);
+
+        if (randomModifier % 2 === 0) {
+            generated = (dec - randomModifier).toString(16);
+        }
+        else {
+            generated = (dec + randomModifier).toString(16);
+        }
+
+        if (validColorRegex.test(generated) && validColors.indexOf(generated) === -1) {
+            validColors.push(generated);
+        }
+        limit--;
+    }
+
+    return JSON.stringify(validColors);
+}
+
+sub.on('message', (channel, message) => {
+    client.hmset('colors', message, findClosestColors(message))
+});
+
+sub.subscribe('insert');
